7b25c7d7e4983cbe2e99c48e02d3ca68ec0b53f0
Sheelah Mogaka <sheelah.mogaka@gmail.com>
2024-07-22 18:33:25 +0300

    smart todo app settings

--- app.py
+++ app.py
@@ -55,5 +55,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.drop_all()
         db.create_all()
-        print("Database tables created successfully")
     app.run(debug=True)
